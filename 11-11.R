831da89c062281118b6274c96cc87cc37f951389
bmwoodruff <bmwoodruff@gmail.com>
2025-11-13 08:52:55 -0700

    11-11

---
+++ 11-11.R
@@ -0,0 +1,28 @@
+x <- c(2,2,2,3,3,5,5,5,5,7,8,8,9,9,9)
+sum(x)
+length(x)
+
+sum(x)/length(x)
+
+values <- c(2,3,5,7,8,9)
+freq <- c(3,2,4,1,2,3)
+
+sum(values * freq)/sum(freq)
+
+prop <- c(3/15,2/15,4/15,1/15,2/15,3/15)
+
+sum(values * prop)
+
+x <- c(-1,3)
+p <- c(7/9,2/9)
+sum(x*p)
+
+
+x <- c(0,1,2,3)
+p <- c(1/8,3/8,3/8,1/8)
+sum(x*p)
+
+x <- c(1,2,3,4,5,6)
+p <- x/sum(x) #me being lafficient.
+sum(x*p)
+
